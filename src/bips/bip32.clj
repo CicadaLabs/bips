7415f14fb45422ef792af58df262ecdc71ffe043
Marius Rabenarivo <mariusrabenarivo@gmail.com>
2022-09-16 12:46:14 +0400

    Uniformize derive-master-node output and add depth

--- src/bips/bip32.clj
+++ src/bips/bip32.clj
@@ -9,15 +9,26 @@
     java.math.BigInteger
     org.web3j.crypto.Sign))
 
-(defn derive-master-code
+(defn derive-master-node
   [seed]
-  (codecs/bytes->hex
-    (mac/hash (codecs/hex->bytes seed) {:key (codecs/str->bytes "Bitcoin seed")
-                                        :alg :hmac+sha512})))
+  (let [master-code
+        (codecs/bytes->hex
+          (mac/hash (codecs/hex->bytes seed) {:key (codecs/str->bytes "Bitcoin seed")
+                                              :alg :hmac+sha512}))
+        private-key (apply str (take 64 master-code))]
+    {:private-key private-key
+     :public-key (codecs/hex->bytes
+                   (.toString (Sign/publicKeyFromPrivate
+                                (BigInteger. (apply str private-key)
+                                             16))
+                              16))
+     :chain-code (apply str (take-last 64 master-code))
+     :depth 0}))
 
 (defn CKDpriv [{k-par :private-key
                 c-par :chain-code
-                i :index}]
+                i :index
+                depth :depth}]
   (let [K-par (codecs/hex->bytes
                 (.toString (Sign/publicKeyFromPrivate
                              (BigInteger. (apply str k-par)
@@ -39,34 +50,40 @@
                                    (.getN Sign/CURVE_PARAMS))
                              16)
      :chain-code (codecs/bytes->hex (byte-array (take-last 32 I)))
-     :index i}))
+     :index i
+     :depth depth}))
 
 (defn CKDpub [{K-par :public-key
                c-par :chain-code
-               i :index}]
+               i :index
+               depth :depth}]
   (if (>= i (math/expt 2 31))
     nil
-    (let [c-K-par (byte-array (codecs/hex->bytes (compress-public-key K-par)))
-          I (mac/hash (codecs/hex->bytes (str (codecs/bytes->hex c-K-par)
+    (let [c-K-par (compress-public-key (byte-array K-par))
+          I (mac/hash (codecs/hex->bytes (str c-K-par
                                               (format "%08x" i)))
                       {:key (codecs/hex->bytes c-par)
-                       :alg :hmac+sha512})]
-      {:public-key (.getEncoded
-                     (.add
-                       (Sign/publicPointFromPrivate (BigInteger. 1 (byte-array (take 32 I))))
-                       (decompressKey (BigInteger. 1
-                                                   (byte-array (take-last 32 c-K-par)))
-                                      (= 0 (mod (nth c-K-par 0) 2))))
-                     false)
+                       :alg :hmac+sha512})
+          public-key (.getEncoded
+                       (.add
+                         (Sign/publicPointFromPrivate
+                           (BigInteger. 1 (byte-array (take 32 I))))
+                         (decompressKey (BigInteger. (apply str c-K-par) 16)
+                                        (= 0 (mod (nth K-par 63) 2))))
+                       false)]
+      {:public-key (byte-array (take-last 64 public-key))
        :chain-code (codecs/bytes->hex (byte-array (take-last 32 I)))
-       :index i})))
+       :index i
+       :depth depth})))
 
 (defn N [{k-par :private-key
           c-par :chain-code
-          i :index}]
+          i :index
+          depth :depth}]
   {:public-key (codecs/hex->bytes
                  (.toString
                    (Sign/publicKeyFromPrivate
                      (BigInteger. (apply str k-par) 16)) 16))
    :chain-code c-par
-   :index i})
+   :index i
+   :depth depth})
--- test/bips/bip32_test.clj
+++ test/bips/bip32_test.clj
@@ -2,7 +2,7 @@
   (:require
     [buddy.core.codecs :as codecs]
     [buddy.core.hash :as hash]
-    [bips.bip32 :refer [derive-master-code CKDpriv CKDpub N]]
+    [bips.bip32 :refer [derive-master-node CKDpriv CKDpub N]]
     [bips.bip32-utils :refer [key-fingerprint serialize]]
     [clojure.math.numeric-tower :as math]
     [clojure.test :refer [deftest is]])
@@ -12,72 +12,62 @@
 
 (deftest derive-master-code-ckd-priv-neutered-test-vector-1
   (let [seed "000102030405060708090a0b0c0d0e0f"
-        master-code (derive-master-code seed)
-        master-secret-key (apply str (take 64 master-code))
-        master-chain-code (apply str (take-last 64 master-code))
-        private-key (serialize :mainnet :private 0 0 0
-                               master-chain-code master-secret-key)
-        master-public-key (codecs/hex->bytes
-                            (.toString (Sign/publicKeyFromPrivate
-                                         (BigInteger. (apply str master-secret-key)
-                                                      16))
-                                       16))
-        public-key (serialize :mainnet :public 0 0 0
-                              master-chain-code master-public-key)
-        child (CKDpriv {:private-key master-secret-key
-                        :chain-code master-chain-code
+        master-node (derive-master-node seed)
+        bip58-encoded-private-key (serialize :mainnet :private 0 0 0
+                                             (:chain-code master-node)
+                                             (:private-key master-node))
+        bip58-encoded-public-key (serialize :mainnet :public 0 0 0
+                                            (:chain-code master-node)
+                                            (:public-key master-node))
+        child (CKDpriv {:private-key (:private-key master-node)
+                        :chain-code (:chain-code master-node)
                         :index (+ (math/expt 2 31) 0)})
-        fingerprint (key-fingerprint master-public-key)
-        formatted-child-private-key (serialize :mainnet :private 1 fingerprint (math/expt 2 31)
-                                               (:chain-code child)
-                                               (:private-key child))
+        fingerprint (key-fingerprint (:public-key master-node))
+        bip58-encoded-child-private-key (serialize :mainnet :private 1 fingerprint (math/expt 2 31)
+                                                   (:chain-code child)
+                                                   (:private-key child))
         neutered (N {:private-key (:private-key child)
                      :chain-code (:chain-code child)
                      :index 1})
-        formatted-neutered-child-public-key (serialize :mainnet :public 1 fingerprint (math/expt 2 31)
-                                                       (:chain-code neutered)
-                                                       (:public-key neutered))]
+        bip58-encoded-neutered-child-public-key (serialize :mainnet :public 1 fingerprint (math/expt 2 31)
+                                                           (:chain-code neutered)
+                                                           (:public-key neutered))]
     (is (= "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi"
-           private-key))
+           bip58-encoded-private-key))
     (is (= "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8"
-           public-key))
+           bip58-encoded-public-key))
     (is (= "xprv9uHRZZhk6KAJC1avXpDAp4MDc3sQKNxDiPvvkX8Br5ngLNv1TxvUxt4cV1rGL5hj6KCesnDYUhd7oWgT11eZG7XnxHrnYeSvkzY7d2bhkJ7"
-           formatted-child-private-key))
+           bip58-encoded-child-private-key))
     (is (= "xpub68Gmy5EdvgibQVfPdqkBBCHxA5htiqg55crXYuXoQRKfDBFA1WEjWgP6LHhwBZeNK1VTsfTFUHCdrfp1bgwQ9xv5ski8PX9rL2dZXvgGDnw"
-           formatted-neutered-child-public-key))))
+           bip58-encoded-neutered-child-public-key))))
 
 (deftest derive-master-code-ckd-priv-pub-test-vector-2
   (let [seed "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542"
-        master-code (derive-master-code seed)
-        master-secret-key (apply str (take 64 master-code))
-        master-chain-code (apply str (take-last 64 master-code))
-        private-key (serialize :mainnet :private 0 0 0
-                               master-chain-code
-                               master-secret-key)
-        master-point-public-key (Sign/publicPointFromPrivate
-                                  (BigInteger. (apply str master-secret-key)
-                                               16))
-        master-public-key (.getEncoded master-point-public-key false)
-        public-key (serialize :mainnet :public 0 0 0
-                              master-chain-code
-                              (byte-array (take-last 64 master-public-key)))
-        child (CKDpriv {:private-key master-secret-key
-                        :chain-code master-chain-code
+        master-node (derive-master-node seed)
+        bip58-encoded-private-key (serialize :mainnet :private 0 0 0
+                                             (:chain-code master-node)
+                                             (:private-key master-node))
+        bip58-encoded-public-key (serialize :mainnet :public 0 0 0
+                                            (:chain-code master-node)
+                                            (:public-key master-node))
+        child (CKDpriv {:private-key (:private-key master-node)
+                        :chain-code (:chain-code master-node)
                         :index 0})
-        fingerprint (key-fingerprint (byte-array (take-last 64 master-public-key)))
-        formatted-child-private-key (serialize :mainnet :private 1 fingerprint 0
-                                               (:chain-code child) (:private-key child))
-        childp (CKDpub {:public-key (byte-array (take-last 64 master-public-key))
-                        :chain-code master-chain-code
+        fingerprint (key-fingerprint (:public-key master-node))
+        bip58-encoded-child-private-key (serialize :mainnet :private 1 fingerprint 0
+                                                   (:chain-code child)
+                                                   (:private-key child))
+        childp (CKDpub {:public-key (:public-key master-node)
+                        :chain-code (:chain-code master-node)
                         :index 0})
-        formatted-child-public-key (serialize :mainnet :public 1 fingerprint 0
-                                              (:chain-code childp)
-                                              (byte-array (take-last 64 (:public-key childp))))]
+        bip58-encoded-child-public-key (serialize :mainnet :public 1 fingerprint 0
+                                                  (:chain-code childp)
+                                                  (:public-key childp))]
     (is (= "xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U"
-           private-key))
+           bip58-encoded-private-key))
     (is (= "xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB"
-           public-key))
+           bip58-encoded-public-key))
     (is (= "xprv9vHkqa6EV4sPZHYqZznhT2NPtPCjKuDKGY38FBWLvgaDx45zo9WQRUT3dKYnjwih2yJD9mkrocEZXo1ex8G81dwSM1fwqWpWkeS3v86pgKt"
-           formatted-child-private-key))
+           bip58-encoded-child-private-key))
     (is (= "xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH"
-           formatted-child-public-key))))
+           bip58-encoded-child-public-key))))
