2fb1802777f99427d5ff783fe7c8a9bf6f9d2d39
Marius Rabenarivo <mariusrabenarivo@gmail.com>
2022-11-07 23:41:46 +0400

    Use buddy.core.codecs in btc-utils

--- src/bips/btc_utils.clj
+++ src/bips/btc_utils.clj
@@ -1,14 +1,10 @@
 (ns bips.btc-utils
   (:require
     [alphabase.base58 :as base58]
+    [buddy.core.codecs :as codecs]
     [clj-commons.digest :as digest])
   (:import
     org.apache.commons.codec.binary.Hex))
-
-(defn hex->bytes
-  "Convert hexadecimal encoded string to bytes array."
-  [^String data]
-  (Hex/decodeHex (.toCharArray data)))
 
 (defn privatekey->wif
   "Convert an hexadecimal encoded private key to WIF"
@@ -19,13 +15,13 @@
         suffix (if (first compressed)
                  "01"
                  "")]
-    (base58/encode (hex->bytes (str (-> private-key
-                                        (#(str prefix % suffix)))
-                                    (-> private-key
-                                        (#(str prefix % suffix))
-                                        (hex->bytes)
-                                        (digest/sha-256)
-                                        (hex->bytes)
-                                        (digest/sha-256)
-                                        (#(take 8 %))
-                                        (#(reduce str %))))))))
+    (base58/encode (codecs/hex->bytes (str (-> private-key
+                                               (#(str prefix % suffix)))
+                                           (-> private-key
+                                               (#(str prefix % suffix))
+                                               (codecs/hex->bytes)
+                                               (digest/sha-256)
+                                               (codecs/hex->bytes)
+                                               (digest/sha-256)
+                                               (#(take 8 %))
+                                               (#(reduce str %))))))))
