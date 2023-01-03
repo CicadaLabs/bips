1bbd581ea99d86d08de2ac51a00b894ee1e312dd
Marius Rabenarivo <mariusrabenarivo@gmail.com>
2023-01-03 16:59:04 +0400

    Correct issue for accessing resources: coin_types.edn

--- src/bips/constants.clj
+++ src/bips/constants.clj
@@ -19,11 +19,12 @@
 
 (ns bips.constants
   (:require
-    [clojure.edn :as edn]))
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]))
 
 ;; coin_types value, which is read from a file at runtime, is used to
 ;; find the coin type with a matching symbol to a `coin-type`.
-(defonce coin-types (edn/read-string (slurp "resources/coin_types.edn")))
+(defonce coin-types (edn/read-string (slurp (io/resource "coin_types.edn"))))
 
 ;; the `:external` and `:change` values representing two possible chain
 ;; types.
