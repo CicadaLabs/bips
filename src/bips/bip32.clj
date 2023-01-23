7c44d1d9fe59565c50e8a005a7cae6400cdacc16
Marius Rabenarivo <mariusrabenarivo@gmail.com>
2023-01-23 11:12:06 +0400

    Fix code style

--- src/bips/bip32.clj
+++ src/bips/bip32.clj
@@ -138,20 +138,20 @@
         parts (gensym 'parts)
         current-node (gensym 'current-node)
         index (gensym 'index)]
-       `(let [~path-parts (str/split ~chain-path #"/")]
-          (loop [~current-node (if (= "m" (first ~path-parts))
-                                 (derive-master-node ~seed)
-                                 (throw (Exception.
-                                         (str "Invalid path: " (first ~path-parts)))))
-                 ~parts (rest ~path-parts)]
-            (if (seq ~parts)
-              (let [~part (first ~parts)
-                    ~index (if (hardened? ~part)
-                             (hardened (Integer/parseInt (subs ~part 0 (- (count ~part) 1))))
-                             (Integer/parseInt ~part))]
-                (recur
-                 (CKDpriv ~current-node ~index)
-                 (rest ~parts)))
-              (if (= :public ~key-type)
-                (N ~current-node)
-                ~current-node))))))
+    `(let [~path-parts (str/split ~chain-path #"/")]
+       (loop [~current-node (if (= "m" (first ~path-parts))
+                              (derive-master-node ~seed)
+                              (throw (Exception.
+                                       (str "Invalid path: " (first ~path-parts)))))
+              ~parts (rest ~path-parts)]
+         (if (seq ~parts)
+           (let [~part (first ~parts)
+                 ~index (if (hardened? ~part)
+                          (hardened (Integer/parseInt (subs ~part 0 (- (count ~part) 1))))
+                          (Integer/parseInt ~part))]
+             (recur
+               (CKDpriv ~current-node ~index)
+               (rest ~parts)))
+           (if (= :public ~key-type)
+             (N ~current-node)
+             ~current-node))))))
